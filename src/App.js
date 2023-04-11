e81a7336e88741df958383a646763f8288e329f4
uddinn9 <uddinn9@gmail.com>
2023-04-11 11:56:33 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Testing my github integration
+          Testing my github integration2
         </p>        
         <a
           className="App-link"
